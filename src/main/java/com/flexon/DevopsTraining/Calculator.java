fefb247a626c3264f00abc2851e6c53711d84fdf
Swathisugumarrajan <swathi.decem88@gmail.com>
2019-02-19 21:53:27 -0800

    Maven in jenkins

---
+++ src/main/java/com/flexon/DevopsTraining/Calculator.java
@@ -0,0 +1,28 @@
+package com.flexon.DevopsTraining;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public Integer numb1,numb2;
+	public static Integer addCalc(Integer numb1,Integer numb2)
+	{
+		if (numb1 == null || numb2 == null)
+			return null;
+		return numb1+numb2;
+		
+	}
+	public static Integer subCalc(Integer numb1,Integer numb2)
+	{
+		if (numb1 == null || numb2 == null)
+			return null;
+		return numb1-numb2;
+	}
+	public static Integer mulCalc(Integer numb1,Integer numb2)
+	{
+		if (numb1 == null || numb2 == null)
+			return null;
+		return numb1*numb2;
+	}
+	
+}
---
+++ src/test/java/com/flexon/DevopsTraining/CalculatorTest.java
@@ -0,0 +1,33 @@
+package com.flexon.DevopsTraining;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+	public void testSum() {
+		
+        System.out.println("sum");
+        Integer x = null;
+        Integer y = null;
+        Integer expResult = 20;
+        Integer result = Calculator.addCalc(x, y);
+        assertEquals(expResult, result);
+        assertEquals(null, Calculator.addCalc(15, null));
+        assertEquals(null, Calculator.addCalc(null, 15));
+        assertEquals(null, Calculator.addCalc(null, null));
+    }
+	@Test
+	public void testDiff() {
+		
+        System.out.println("Difference");
+        Integer x = 55;
+        Integer y = 15;
+        Integer expResult = 30;
+        Integer result = Calculator.subCalc(x, y);
+        assertEquals(expResult, result);
+        assertEquals(null, Calculator.subCalc(15, null));
+        assertEquals(null, Calculator.subCalc(null, 15));
+        assertEquals(null, Calculator.subCalc(null, null));
+    }
+}
+
